0cf89f0560754896af03791e384c322417379493
ccw <bbsuu@126.com>
2012-11-05 23:23:26 +0800

    提交领域模型

---
+++ NPC.Domain/Models/ArticleCategories/ArticleCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fluent.Infrastructure;
+
+namespace NPC.Domain.Models.ArticleCategories
+{
+    public class ArticleCategory : IAggregateRoot
+    {
+        public Guid Id { get; set; }
+        public string CategoryName { get; set; }
+        public string DateOfPublish { get; set; }
+        public string Publisher { get; set; }
+        public string PublisherId { get; set; }
+    }
+}
---
+++ NPC.Domain/Models/Articles/Article.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fluent.Infrastructure;
+
+namespace NPC.Domain.Models.Articles
+{
+    public class Article : IAggregateRoot
+    {
+        public virtual string Title { get; set; }
+        public virtual string Content { get; set; }
+        public virtual string Publisher { get; set; }
+        public virtual Guid PublisherId { get; set; }
+        public virtual DateTime DateOfPublish { get; set; }
+        public virtual int HitCount { get; set; }
+    }
+}
