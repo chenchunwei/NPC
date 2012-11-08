4dfbed08e8a2555b13bc8c675e634610231a7de4
ccw <bbsuu@126.com>
2012-11-08 23:45:52 +0800

    添加静态页面模板

---
+++ NPC.Website.Manage/Controllers/UnitsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NPC.Website.Manage.Controllers
+{
+    public class UnitsController : Controller
+    {
+        //
+        // GET: /Units/
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+    }
+}
